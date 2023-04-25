9732a2892372e42b5ac94dbb90b51dc593cf4e4a
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-04-25 11:57:33 -0300

    Update Ave_Voadora.java

--- mps_labsolid/04_ave/Ave_Voadora.java
+++ mps_labsolid/04_ave/Ave_Voadora.java
@@ -1,3 +1,7 @@
 public interface Ave_Voadora {
     public void voa();
 }
+/* 
+Aplicou-se o princípio de Separação de Interfaces pois percebeu-se que a classe Pinguim não usa de forma devida a Interface original Ave, devido a um dos métodos que ela possuia originalmente
+Optou-se então por criar uma nova interface, modificando-se a original e a implementação delas pelas classes Pinguim e Águia
+*/  
